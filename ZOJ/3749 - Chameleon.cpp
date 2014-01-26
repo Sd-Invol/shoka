97845990258b8a975e7ebd2769533cc1de92b443
Sd-Invol <Sd.Invol@gmail.com>
2014-01-26 19:51:22 +0800

    update

--- ZOJ/3749 - Chameleon.cpp
+++ ZOJ/3749 - Chameleon.cpp
@@ -1,17 +1,17 @@
-#include iostream
-#include cstdio
-#include cstdlib
-#include cstring
-#include algorithm
-#include vector
-#include cmath
-#include map
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <map>
 using namespace std;
 typedef long long LL;
 #define N 100005
 #define M 105
 int n , m , sum , s[N] , block = 1000;
-vectorint a[N];
+vector<int> a[N];
 int d[N] , D , ans;
 int id[N] , cnt;
 int res[M][N] , rank[N];
@@ -20,61 +20,61 @@ void work()
 {
     int i , j , k , x , y;
     sum = D = 0;
-    for (i = 1 ; i = n ; ++ i)
+    for (i = 1 ; i <= n ; ++ i)
     {
-        scanf(%d,&s[i]) , a[i].resize(s[i]) , sum += s[i];
-        for (j = 0 ; j  s[i] ; ++ j)
-            scanf(%d,&a[i][j]) , d[D ++] = a[i][j];
+        scanf("%d",&s[i]) , a[i].resize(s[i]) , sum += s[i];
+        for (j = 0 ; j < s[i] ; ++ j)
+            scanf("%d",&a[i][j]) , d[D ++] = a[i][j];
         sort(a[i].begin() , a[i].end());
     }
     sort(d , d + D) , D = unique(d , d + D) - d;
-    for (i = 1 ; i = n ; ++ i)
-        for (j = 0 ; j  s[i] ; ++ j)
+    for (i = 1 ; i <= n ; ++ i)
+        for (j = 0 ; j < s[i] ; ++ j)
             a[i][j] = lower_bound(d , d + D , a[i][j]) - d + 1;
     cnt = 0;
 
-    for (i = 1 ; i = n ; ++ i)
-        if (s[i] = block)
+    for (i = 1 ; i <= n ; ++ i)
+        if (s[i] >= block)
         {
             id[i] = ++ cnt;
             memset(rank , 0 , sizeof(rank));
-            for (j = 1 , k = 0 ; j = D ; ++ j)
+            for (j = 1 , k = 0 ; j <= D ; ++ j)
             {
-                while (k  s[i] && j = a[i][k])
+                while (k < s[i] && j >= a[i][k])
                     ++ k;
                 rank[j] = k;
             }
             memset(res[cnt] , 0 , sizeof(res[cnt]));
-            for (j = 1 ; j = n ; ++ j)
-                if (i != j && s[i] = s[j])
+            for (j = 1 ; j <= n ; ++ j)
+                if (i != j && s[i] >= s[j])
                 {
-                    for (k = 0 ; k  s[j] ; ++ k)
+                    for (k = 0 ; k < s[j] ; ++ k)
                         d[k] = rank[a[j][k]];
                     x = unique(d , d + s[j]) - d;
-                    for (k = 0 ; k  x ; ++ k)
-                        if (d[k] == 0  d[k] == s[i])
+                    for (k = 0 ; k < x ; ++ k)
+                        if (d[k] == 0 || d[k] == s[i])
                             ++ res[cnt][j];
                         else
                             res[cnt][j] += 2;
                 }
         }
-    scanf(%d,&m);
+    scanf("%d",&m);
     while (m --)
     {
-        scanf(%d%d,&x,&y);
-        if (s[x]  s[y])
+        scanf("%d%d",&x,&y);
+        if (s[x] > s[y])
             swap(x , y);
-        if (s[y]  block)
+        if (s[y] < block)
         {
             i = j = ans = 0;
             bool pre;
-            if (a[x][i]  a[y][j])
+            if (a[x][i] < a[y][j])
                 ++ i , pre = 0;
             else
                 ++ j , pre = 1;
-            while (i  s[x]  j  s[y])
+            while (i < s[x] || j < s[y])
             {
-                if (j = s[y]  (i  s[x] && a[x][i]  a[y][j]))
+                if (j >= s[y] || (i < s[x] && a[x][i] < a[y][j]))
                 {
                     ++ i;
                     if (pre != 0)
@@ -89,27 +89,16 @@ void work()
                     pre = 1;
                 }
             }
-            printf(%dn , ans);
+            printf("%d\n" , ans);
         }
         else
-        {
-            ans = 0;
-            for (i = 0 ; i  s[x] ; ++ i)
-                d[i] = rank[id[y]][a[x][i]];
-            D = unique(d , d + s[x]) - d;
-            for (i = 0 ; i  D ; ++ i)
-                if (d[i] == 0  d[i] == s[y])
-                    ++ ans;
-                else
-                    ans += 2;
-            printf(%dn , res[id[y]][x]);
-        }
+            printf("%d\n" , res[id[y]][x]);
     }
 }
 
 int main()
 {
-    while (~scanf(%d,&n))
+    while (~scanf("%d",&n))
         work();
     return 0;
 }
