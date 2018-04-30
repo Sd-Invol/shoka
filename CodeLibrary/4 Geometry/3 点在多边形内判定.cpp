9bc83cb967b2451cc5a5e69741612d48a26bbacb
Xuanang Zhao <Sd.Invol@gmail.com>
2018-04-30 23:33:47 +0800

    Add stab in polygon

--- CodeLibrary/4 Geometry/3 点在多边形内判定.cpp
+++ CodeLibrary/4 Geometry/3 点在多边形内判定.cpp
@@ -14,3 +14,19 @@ int PointInPolygon(Point P , const Point *p , int np) {
     }
     return res;
 }
+
+vector<pair<double, int>> StabPolygonLength(Point P, Point V, const Point *p, int np) {
+    auto arg = [&P, &V](Point Q, Point W) {
+        return (W ^ (P - Q)) / (V ^ W);
+    };
+    vector<pair<double, int>> events;
+    for (int i = 0 ; i < np ; ++ i) {
+        int d0 = dcmp((p[i] - P) ^ V), d1 = dcmp((p[(i + 1) % np] - P) ^ V);
+        if (d0 == d1) continue;
+        double t = arg(p[i] , p[(i + 1) % np] - p[i]);
+        int sign = d0 < d1 ? 1 : -1;
+        events.emplace_back(t , sign * (d0 && d1 ? 2 : 1));
+    }
+    sort(events.begin() , events.end());
+    return events;
+}
