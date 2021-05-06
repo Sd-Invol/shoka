ec43e08a3f6eaa54f64e0beda19b4ee369ab130a
Sd-Invol <sd.invol@gmail.com>
2021-05-06 23:33:36 +0800

    Update 1 Tarjan.cpp

--- CodeLibrary/2 Graph Theory/1 Tarjan.cpp
+++ CodeLibrary/2 Graph Theory/1 Tarjan.cpp
@@ -1,61 +1,71 @@
-int ncnt , scnt , bel[N] , low[N] , dfn[N];
-int f[N];
-stack<int> S;
-void dfs(int x) {
-    low[x] = dfn[x] = ++ ncnt;
-    f[x] = 1;
-    S.push(x);
-    for (int i = pre[x] ; ~i ; i = e[i].next) {
-        int y = e[i].x;
-        if (!dfn[y]) {
-            dfs(y);
-            low[x] = min(low[x] , low[y]);
-        } else if (f[y]) {
-            low[x] = min(low[x] , dfn[y]);
-        }
+namespace tarjan {
+int low[N], ord[N];
+int scnt, bel[N];
+bool in_stack[N];
+std::stack<int> stack;
+void dfs(int x, int &&ncnt) {
+  low[x] = ord[x] = ++ncnt;
+  in_stack[x] = 1;
+  stack.push(x);
+  for (auto &y : e[x]) {
+    if (!ord[y]) {
+      dfs(y, std::move(ncnt));
+      low[x] = std::min(low[x], low[y]);
+    } else if (in_stack[y]) {
+      low[x] = std::min(low[x], ord[y]);
     }
-    if (low[x] == dfn[x]) {
-        do {
-            int i = S.top();
-            S.pop();
-            f[i] = 0;
-            bel[i] = scnt;
-        } while (i != x);
-        ++ scnt;
+  }
+  if (low[x] == ord[x]) {
+    while (true) {
+      int i = stack.top();
+      stack.pop();
+      in_stack[i] = false;
+      bel[i] = scnt;
+      if (i == x) {
+        break;
+      }
     }
+    ++scnt;
+  }
+}
+void GetSCC() {
+  std::fill(ord, ord + n, 0);
+  scnt = 0;
+  for (int i = 0; i < n; ++i) {
+    if (!ord[i]) {
+      dfs(i, 0);
+    }
+  }
 }
 
-memset(dfn , 0 , sizeof(dfn));
-ncnt = scnt = 0;
-for (int i = 1 ; i <= n ; ++ i) {
-    !dfn[i] && dfs(i;
-}
-                    
+}  // namespace tarjan
+
 /***********************************************/
-int dfn[N] , low[N] , ncnt;
+int dfn[N], low[N], ncnt;
 stack<int> S;
-int bel[M] , tmp[N];
-void dfs(int x , int fa) {
-    dfn[x] = low[x] = ++ ncnt;
-    for (int i = pre[x] ; ~i ; i = e[i].next) {
-        int y = e[i].x;
-        if (!dfn[y]) {
-            S.push(i);
-            dfs(y , i ^ 1);
-            low[x] = min(low[x] , low[y]);
-            if (low[y] > dfn[x]) {}//(x , y) is bridge
-            if (low[y] >= dfn[x]) {
-                ++ n; int j;
-                do {
-                    j = S.top() , S.pop();
-                    if (tmp[e[j].x] != n)
-                        E[m ++] = make_pair(n , e[j].x) , tmp[e[j].x] = n;
-                    if (tmp[e[j ^ 1].x] != n)
-                        E[m ++] = make_pair(n , e[j ^ 1].x) , tmp[e[j ^ 1].x] = n;
-                    bel[j >> 1] = n;
-                } while (j != i);
-            }
-        } else if (i != fa && dfn[y] < dfn[x])
-            S.push(i) , low[x] = min(low[x] , dfn[y]);
-    }
+int bel[M], tmp[N];
+void dfs(int x, int fa) {
+  dfn[x] = low[x] = ++ncnt;
+  for (int i = pre[x]; ~i; i = e[i].next) {
+    int y = e[i].x;
+    if (!dfn[y]) {
+      S.push(i);
+      dfs(y, i ^ 1);
+      low[x] = min(low[x], low[y]);
+      if (low[y] > dfn[x]) {
+      }  //(x , y) is bridge
+      if (low[y] >= dfn[x]) {
+        ++n;
+        int j;
+        do {
+          j = S.top(), S.pop();
+          if (tmp[e[j].x] != n) E[m++] = make_pair(n, e[j].x), tmp[e[j].x] = n;
+          if (tmp[e[j ^ 1].x] != n)
+            E[m++] = make_pair(n, e[j ^ 1].x), tmp[e[j ^ 1].x] = n;
+          bel[j >> 1] = n;
+        } while (j != i);
+      }
+    } else if (i != fa && dfn[y] < dfn[x])
+      S.push(i), low[x] = min(low[x], dfn[y]);
+  }
 }
