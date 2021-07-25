71f029ea6b326c1a9f7febb932f1620835942ce0
Sd-Invol <sd.invol@gmail.com>
2021-07-25 16:06:51 +0800

    Update some frequently used code

---
+++ CodeLibrary/1 Data Structure/11 线段树.cpp
@@ -0,0 +1,82 @@
+namespace SegTree {
+inline int id(int l, int r) {
+  return l + r | l != r;
+}
+#define Left l, mid
+#define Right mid + 1, r
+typedef std::pair<int, int> Node;
+struct stree {
+  Node w;
+  int f;
+  void add(int x) {
+    f += x;
+    w.first += x;
+  }
+} t[N << 1];
+Node merge(const Node& A, const Node B) {
+  Node C(std::min(A.first, B.first), 0);
+  if (C.first == A.first) {
+    C.second += A.second;
+  }
+  if (C.first == B.first) {
+    C.second += B.second;
+  }
+  return C;
+}
+void pushup(int p, int l, int mid, int r) {
+  t[p].w = merge(t[id(Left)].w, t[id(Right)].w);
+}
+void pushdown(int p, int l, int mid, int r) {
+  if (t[p].f) {
+    t[id(Left)].add(t[p].f);
+    t[id(Right)].add(t[p].f);
+    t[p].f = 0;
+  }
+}
+void add(int l, int r, int top, int bot, int w) {
+  int p = id(l, r);
+  if (top <= l && r <= bot) {
+    t[p].add(w);
+  } else {
+    int mid = l + r >> 1;
+    pushdown(p, l, mid, r);
+    if (top <= mid) {
+      add(Left, top, bot, w);
+    }
+    if (bot > mid) {
+      add(Right, top, bot, w);
+    }
+    pushup(p, l, mid, r);
+  }
+}
+Node query(int l, int r, int top, int bot) {
+  int p = id(l, r);
+  if (top <= l && r <= bot) {
+    return t[p].w;
+  } else {
+    int mid = l + r >> 1;
+    pushdown(p, l, mid, r);
+    if (bot <= mid) {
+      return query(Left, top, bot);
+    }
+    if (bot > mid) {
+      return query(Right, top, bot);
+    }
+    return merge(query(Left, top, bot), query(Right, top, bot));
+  }
+}
+void build(int l, int r) {
+  int p = id(l, r);
+  t[p].f = 0;
+  if (l == r) {
+    t[p].w = Node(0, a[l]);
+  } else {
+    int mid = l + r >> 1;
+    build(Left);
+    build(Right);
+    pushup(p, l, mid, r);
+  }
+}
+#undef Left
+#undef Right
+}  // namespace SegTree
--- CodeLibrary/1 Data Structure/5 树状数组.cpp
+++ CodeLibrary/1 Data Structure/5 树状数组.cpp
@@ -1,41 +1,68 @@
-int getKth(int k) {
-    int x = 0 , i;
-    for (i = 16 ; i >= 0 ; -- i)
-        if (x + (1 << i) <= D && c[x + (1 << i)] < k) {
-            x += 1 << i;
-            k -= c[x];
-        }
-    return x + 1;
-}
+template <typename T>
+class FenwickTree {
+ public:
+  int n;
+  std::vector<T> a;
+  FenwickTree(int n) : n(n) {
+    a.resize(n);
+  }
+  void add(int x, T w) {
+    while (x < n) {
+      a[x] += w;
+      x |= (x + 1);
+    }
+  }
+  T query(int x) {
+    T res{};
+    x = std::min(x, n - 1);
+    while (x >= 0) {
+      res += a[x];
+      x = (x & (x + 1)) - 1;
+    }
+    return res;
+  }
+  std::pair<int, T> kth(T k) {
+    int x = -1;
+    for (int i = 20; i >= 0; --i) {
+      int y = x + (1 << i);
+      if (y < n && a[y] < k) {
+        k -= a[y];
+        x = y;
+      }
+    }
+    return {x + 1, k};
+  }
+};
 struct CHU_2_BIT {
-    int n;
-    LL B[N] , C[N];
-    void init(int size) {
-        n = size;
-        memset(B , 0 , n + 1 << 3);
-        memset(C , 0 , n + 1 << 3);
-    }
-    CHU_2_BIT() {}
-    CHU_2_BIT(int size) {
-        init(size);
-    }
-    inline LL _sum(LL* c , int x) {
-        LL res = 0;
-        for ( ; x > 0 ; x -= x & -x)
-            res += c[x];
-        return res;
-    }
-    void add(int l , int r , LL w) {
-        for (int i = l ; i <= n ; i += i & -i)
-            B[i] += w , C[i] += w * l;
-        ++ r;
-        for (int i = r ; i <= n ; i += i & -i)
-            B[i] -= w , C[i] -= w * r;
-    }
-    LL sum(int l , int r) {
-        LL res = 0; -- l;
-        res += (r + 1) * _sum(B , r) - _sum(C , r);
-        res -= (l + 1) * _sum(B , l) - _sum(C , l);
-        return res;
-    }
-}T;
+  int n;
+  LL B[N], C[N];
+  void init(int size) {
+    n = size;
+    memset(B, 0, n + 1 << 3);
+    memset(C, 0, n + 1 << 3);
+  }
+  CHU_2_BIT() {}
+  CHU_2_BIT(int size) {
+    init(size);
+  }
+  inline LL _sum(LL* c, int x) {
+    LL res = 0;
+    for (; x > 0; x -= x & -x)
+      res += c[x];
+    return res;
+  }
+  void add(int l, int r, LL w) {
+    for (int i = l; i <= n; i += i & -i)
+      B[i] += w, C[i] += w * l;
+    ++r;
+    for (int i = r; i <= n; i += i & -i)
+      B[i] -= w, C[i] -= w * r;
+  }
+  LL sum(int l, int r) {
+    LL res = 0;
+    --l;
+    res += (r + 1) * _sum(B, r) - _sum(C, r);
+    res -= (l + 1) * _sum(B, l) - _sum(C, l);
+    return res;
+  }
+} T;
