42a3606792049b9ab0cdb80a1d289e93d5a2e6e4
Sd-Invol <sd.invol@gmail.com>
2021-08-22 14:44:07 +0800

    Update Lagrange function mod P^e

--- CodeLibrary/3 Mathematics/9 拉格朗日插值.cpp
+++ CodeLibrary/3 Mathematics/9 拉格朗日插值.cpp
@@ -1,56 +1,71 @@
-for (int i = 0 ; i < n ; ++ i)
-    val[i] = cal(i);
-for (int i = 0 ; i < n ; ++ i) {
-    int cur = 0 , nxt = 1;
-    memset(f[cur] , 0 , sizeof(f[cur]));
-    f[cur][0] = 1;
-    for (int j = 0 ; j < n ; ++ j) {
-        if (i != j) {
-            x = inverse((i - j + Q) % Q);
-            y = (LL)(Q - j) * x % Q;
-            //printf("%d %d : %d %d\n" , i , j , x , y);
-            memset(f[nxt] , 0 , sizeof(f[nxt]));
-            for (int k = 0 ; k < n ; ++ k) {
-                if (f[cur][k]) {
-                    f[nxt][k] += (LL)f[cur][k] * y % Q;
-                    f[nxt][k] %= Q;
-                    f[nxt][k + 1] += (LL)f[cur][k] * x % Q;
-                    f[nxt][k + 1] %= Q;
-                }
-            }
-            swap(cur , nxt);
-        }
+// a : F(0), F(1) .. F(n - 1) mod P^e => F(u) mod P^e
+int64 Lagrange(const std::vector<int64>& a, int64 u, int P, int e = 1) {
+  // mod P^e // O(n(e + log))
+  typedef std::pair<int64, int> Pint;  // first * P ^ second
+  if (u < a.size()) {
+    return a[u];
+  }
+  int64 Pe = 1, n = a.size();
+  for (int i = 0; i < e; ++i) {
+    Pe *= P;
+  }
+  std::function<void(Pint&, int64)> multi = [&](Pint& A, int64 x) {
+    while (x && x % P == 0) {
+      x /= P;
+      ++A.second;
     }
-    for (int j = 0 ; j < n ; ++ j)
-        L[i][j] = f[cur][j];
+    A.first = A.first * x % Pe;
+  };
+  Pint numerator = {1, 0};
+  for (int i = 0; i < n; ++i) {
+    multi(numerator, u - i);
+  }
+  std::vector<Pint> fac(n);
+  fac[0] = {1, 0};
+  for (int i = 1; i < n; ++i) {
+    fac[i] = fac[i - 1];
+    multi(fac[i], i);
+  }
+  int64 res = 0;
+  for (int i = 0; i < n; ++i) {
+    Pint denominator = fac[i];
+    denominator.second += fac[n - i - 1].second;
+    denominator.first = denominator.first * fac[n - i - 1].first % Pe;
+    multi(denominator, u - i);
+    if (numerator.second - denominator.second < e) {
+      int64 delta =
+          numerator.first * power(denominator.first, Pe / P - 1, Pe) % Pe;
+      delta = delta * power(P, numerator.second - denominator.second, Pe) % Pe;
+      if ((n - i - 1) & 1) {
+        delta = Pe - delta;
+      }
+      res = (res + delta * a[i] % Pe) % Pe;
+    }
+  }
+  return res;
 }
-memset(res , 0 , sizeof(res));
-for (int i = 0 ; i < n ; ++ i)
-    for (int x = 0 ; x < n ; ++ x) {
-        res[x] += (LL)L[i][x] * val[i] % Q;
-        res[x] %= Q;
-    }
 /******************************************/
 // 特殊的，一个K阶多项式已知前K+1项求第n项 O(Klogn)
 // 例: 求前n个自然数的K次方和，多项式是K+1阶
-scanf("%d%d" , &n , &K);
-for (int i = 1 ; i <= K + 1 ; ++ i) {
-    f[i] = (f[i - 1] + power(i , K)) % Q;
+scanf("%d%d", &n, &K);
+for (int i = 1; i <= K + 1; ++i) {
+  f[i] = (f[i - 1] + power(i, K)) % Q;
 }
 if (n <= K + 1) {
-    printf("%d\n" , f[n]);
-    return;
+  printf("%d\n", f[n]);
+  return;
 }
-int A = 1 , B = 1;
-for (int i = 0 ; i <= K + 1; ++ i)
-    A = (LL)A * (n - i) % Q;
-for (int i = 1 ; i <= K + 1; ++ i)
-    B = (LL)B * (Q - i) % Q;
+int A = 1, B = 1;
+for (int i = 0; i <= K + 1; ++i)
+  A = (LL)A * (n - i) % Q;
+for (int i = 1; i <= K + 1; ++i)
+  B = (LL)B * (Q - i) % Q;
 int res = 0;
-for (int i = 0 ; i <= K + 1; ++ i) {
-    int C = (LL)A * inverse((LL)(n - i) * B % Q) % Q;
-    res += (LL)f[i] * C % Q , res %= Q;
-    if (i == K + 1) break;
-    B = (LL)B * (i + 1) % Q * inverse(Q - (K + 1 - i)) % Q;
+for (int i = 0; i <= K + 1; ++i) {
+  int C = (LL)A * inverse((LL)(n - i) * B % Q) % Q;
+  res += (LL)f[i] * C % Q, res %= Q;
+  if (i == K + 1)
+    break;
+  B = (LL)B * (i + 1) % Q * inverse(Q - (K + 1 - i)) % Q;
 }
-printf("%d\n" , res);
+printf("%d\n", res);
